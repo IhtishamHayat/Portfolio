f94263a0bff5acbd79a4137303a77679017edb76
Ihtisham Hayat <ihtishamhayat@gmail.com>
2023-12-10 00:52:12 +0500

    updated portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,8 @@ const App = () => {
       <Hireme />
       <Contact />
       <footer className="p-3 text-center">
-        <h6 className="mb-3">JOHN ALEX</h6>
-        <p>codeaprogram © All CopyRights Reserved 2022</p>
+        <h6 className="mb-3">Ihtisham Hussain</h6>
+        <p>heythereahti © All CopyRights Reserved 2023</p>
       </footer>
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -15,7 +15,7 @@ import services_logo3 from "./assets/images/Services/logo3.png";
 import pakeeza from "./assets/images/projects/pakeeza.png";
 import social from "./assets/images/projects/social.png";
 import creative from "./assets/images/projects/creative.png";
-import person_project from "./assets/images/projects/person.png";
+import person from "./assets/images/projects/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
 import avatar2 from "./assets/images/Testimonials/avatar2.png";
