9a9ca635a063860e04542714be28504f57e41fc7
Ihtisham Hayat <ihtishamhayat@gmail.com>
2023-12-10 00:41:22 +0500

    updates

--- src/Content.js
+++ src/Content.js
@@ -12,9 +12,9 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/pakeeza.png";
-import project2 from "./assets/images/projects/Social.png";
-import project3 from "./assets/images/projects/creative.png";
+import pakeeza from "./assets/images/projects/pakeeza.png";
+import social from "./assets/images/projects/social.png";
+import creative from "./assets/images/projects/creative.png";
 import person_project from "./assets/images/projects/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
